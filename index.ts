f616b009fb81af05331c64e62ee30c386a454fe6
FrancisUzoigwe <kossyuzoigwe@gmail.com>
2023-11-10 15:21:09 +0100

    process

--- index.ts
+++ index.ts
@@ -7,3 +7,16 @@ const port: number = 3400;
 const Server = app.listen(port, () => {
   console.log("Server is live on port", port);
 });
+
+process.on("uncaughtException", (error) => {
+  console.log("Server is shutting down due to an uncaught exception", error);
+  process.exit(1);
+});
+
+process.on("unhandledRejection", (reason) => {
+  console.log("Server is shutting down due to an unhandled rejection", reason);
+
+  Server.close(() => {
+    process.exit(1);
+  });
+});
